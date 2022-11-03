ef2a2286193bd0d6c0ae5506c7326e56e04528c0
jlangaa <60393950+jlangaa@users.noreply.github.com>
2022-11-03 10:26:10 -0400

    Update prorated_scoring.R
    added prorated sum function with mean and median imputation

--- prorated_scoring.R
+++ prorated_scoring.R
@@ -2,7 +2,7 @@
 # author: "Joshua Langfus"
 # date: "2022-11-03"
 
-# This file contains function useful for doing prorated scoring of item-level data in R. This is meant to roughly replicate SPSS syntax such as, e.g.,  MEAN.5 (which computes a mean as long as there are 5 valid observations).
+# This file contains function useful for doing prorated scoring of item-level data in R. This is meant to roughly replicate SPSS syntax such as, e.g.,  MEAN.5 (which computes a mean as long as there are 5 valid observations). Functions are written in base R whenever possible, with additional package requirements noted when possible. Functions are documented in Roxygen syntax for consistency.
 
 #' Compute a prorated mean for a give number of valid observations
 #'
@@ -27,3 +27,35 @@ mean.n <- function(x, n) {
     return(mean(x, na.rm =TRUE))
   }
 }
+
+#' Compute a prorated sum for a give number of valid observations
+#'
+#' This function mimics the behavior of SPSS SUM.N statements. A sum
+#' is computed as long as N valid items are present in the input vector
+#' and prorated (with mean or median imputation) to to the full length of x
+#'
+#' @param x A numeric vector for which to take the sum
+#' @param n, A scalar indicating the minimum number of valid observations
+#' @param impute A character vector indicating either "mean" (default) or "median" imputation of missing values.
+#' @param ... Additional arguments (not yet implemented)
+#'
+#' @return A numeric, the prorated sum of x
+#' @export
+#' @examples
+#' d <- data.frame(x = c(1,2,3,NA,5), y = c(1, 3, NA, NA, 5))
+#' d %>% summarise(across(everything(), sum.n, n=4))
+
+sum.n <- function(x, n, impute = "mean") {
+  n.miss <- sum(is.na(x))
+  if ((length(x) - n.miss) < n) {
+    return(NA_real_)
+    
+  } else if (impute == "mean") {
+      m <- mean(x, na.rm = TRUE)
+  } else if (impute == "median") {
+      m <- median(x, na.rm = TRUE)
+  }
+  x[is.na(x)] <- m
+  return(mean(x))
+}
+
